2a96f988149e73777f7dd346ef135fa1fe13abdb
Lordfedot <orestmazgal@gmail.com>
2022-12-14 23:10:30 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,26 @@
-export const App = () => {
-  return (
-    <>
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Searchbar } from './Searchbar/Searchbar';
+export class App extends Component {
+  state = {
+    name: '',
+  };
+  handleFormSubmit = name => {
     
-    </>
-  );
-};
+    this.setState({ name });
+  };
+  render() {
+    return (
+      <>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ImageGallery name={this.state.name} />
+      </>
+    );
+  }
+}
+
+App.propTypes ={
+  onSubmit: PropTypes.func,
+  name: PropTypes.string,
+}
